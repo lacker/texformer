1bb0a16705005618c166e22beec472dded691dee
Kevin Lacker <lacker@gmail.com>
2020-10-23 11:10:53 -0700

    3e-4

--- captions.py
+++ captions.py
@@ -129,7 +129,7 @@ def train():
     conf = TrainerConfig(
         max_epochs=epochs,
         batch_size=4,
-        learning_rate=6e-5,
+        learning_rate=3e-4,
         lr_decay=False,
         warmup_tokens=tokens_per_epoch,
         final_tokens=epochs * tokens_per_epoch,
